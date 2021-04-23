685610e9cfb5cd61a2c0cca2978407b873f41ae5
mad-rainbow <staspronumber13@gmail.com>
2021-04-23 19:45:06 +0300

    Update Task02.java

--- src/lt/lhu/unit03/main/Task02.java
+++ src/lt/lhu/unit03/main/Task02.java
@@ -1,6 +1,5 @@
 package lt.lhu.unit03.main;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Task02 {
@@ -8,21 +7,28 @@ public class Task02 {
 	public static void main(String[] args) {
 		int[] ar = new int[5];
 		Random rand = new Random();
+		int min, max;
 		
 		for (int i = 0; i < ar.length; i++) {
 			ar[i] = rand.nextInt(1000);
 		}
+		min = ar[0];
+		max = ar[0];
 		
 		for (int el : ar) {
 			System.out.printf("[%3d] ", el);
+			if (min > el) {
+				min = el;
+			}
+			if (max < el) {
+				max = el;
+			}
 		}
 		System.out.println("\n-----------------------------");
 		
-		Arrays.sort(ar);
+		int axisLength = max - min;
 		
-		int min = ar[1] - ar[0];
-		
-		System.out.println("Наименьшая длина числовой оси = " + min);
+		System.out.println("Наименьшая длина числовой оси = " + axisLength);
 		
 
 	}
